1173d93640625450357921baecfc835bd0fbc4a0
Jeffrey Alex Vaughn <vaughnj29@gmail.com>
2022-04-11 16:17:11 -0700

    Adding Redux Store, Provider & connect

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { Provider, connect } from 'react-redux';
+import { Provider} from 'react-redux';
+import { createStore } from 'redux';
+import { searchRobots } from './reducers';
 import 'tachyons';
 import App from './containers/App';
+
+const store = createStore( searchRobots );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    
   </React.StrictMode>
 );
 
