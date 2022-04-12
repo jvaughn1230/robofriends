a45f67982653af01af69df8cf9fd56516fe1e9d3
Jeffrey Alex Vaughn <vaughnj29@gmail.com>
2022-04-12 15:06:34 -0700

    adding redux-logger and redux-thunk

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { Provider} from 'react-redux';
-import { createStore } from 'redux';
-import { searchRobots } from './reducers';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { searchRobots, requestRobots } from './reducers';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import 'tachyons';
 import App from './containers/App';
 
-const store = createStore( searchRobots );
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
+const store = createStore( rootReducer, applyMiddleware( thunkMiddleware, logger) );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
